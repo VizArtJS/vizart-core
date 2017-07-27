e352d10edcfbd44e0f1a64baa1592fd9d3df447f
Xing Yun <ink.xing.yun@gmail.com>
2017-07-27 14:46:39 -0400

    new default categorical color, bump to 0.6.1

--- src/options/DefaultCategoricalColor.js
+++ src/options/DefaultCategoricalColor.js
@@ -1,5 +1,7 @@
 const DefaultCategoricalColor = {
-    scheme: ['#E5EEC1','#A2D4AB','#3EACA8','#547A82','#5A5050'],
+    scheme: ['#3b73b9', '#aa71aa', '#ee3224', '#00aeef', '#f8981d',
+        '#80bb42', '#aacdef', '#cacec2', '#aaa6ce', '#df9e9d', '#6ab2e7',
+        '#ffdd00', '#9ac2b9', '#a7a9ac', '#bbbb42', '#e6a6ce'],
     type: 'categorical'
 };
 
