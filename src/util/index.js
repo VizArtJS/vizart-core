67bb368e1282859872e70b87a53da5d554963d8d
Xing Yun <ink.xing.yun@gmail.com>
2017-08-04 13:20:06 -0400

    drop useless constants

--- src/util/index.js
+++ src/util/index.js
@@ -3,5 +3,4 @@ export * from './container-size';
 export { default as mobileAndTabletCheck } from './mobile-check';
 export { default as truncate } from './svg-text-truncate';
 export * from './svg-text-wrap';
-export { default as Duration } from './transition';
 export { default as uuid } from './uuid';
