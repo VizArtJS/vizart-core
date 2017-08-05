edbe084177854a6ec50b1a101deaa521598f5d32
Xing Yun <ink.xing.yun@gmail.com>
2017-08-04 20:41:46 -0400

    fix unit test

--- test/colorscale_test.js
+++ test/colorscale_test.js
@@ -1,11 +1,11 @@
-import { reduceToPalette, IceCube, getSubColorSpace, HclSelector } from '../src/color';
+import { reduceToPalette, ColorPreset, getSubColorSpace, HclSelector } from '../src/color';
 import { strictEqual } from 'assert';
 
 describe('color scale', () => {
   it('make a gradient scale', () => {
-      let palette = reduceToPalette(6, IceCube);
+      let palette = reduceToPalette(6, ColorPreset.Hcl.IceCube);
       console.log(palette);
-      let _selector = new HclSelector(IceCube);
+      let _selector = new HclSelector(ColorPreset.Hcl.IceCube);
 
 
       let sub = getSubColorSpace(palette, function (c) {
