ceb65d0affd55a5e62e9f7073043942102f41ccf
Xing Yun <ink.xing.yun@gmail.com>
2017-10-09 15:36:19 +0800

    add canvas support

---
+++ src/canvas/helper/index.js
@@ -0,0 +1,11 @@
+import drawCircularText from './draw-circular-text';
+import genColorByIndex from './generate-color';
+import radialGradient from './radial-gradient';
+import verticalGradient from './vertical-gradiet';
+
+export {
+    drawCircularText,
+    genColorByIndex,
+    radialGradient,
+    verticalGradient
+}
