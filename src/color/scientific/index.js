11f7fc6d8e841dd0b03452cad33949a4adb204b4
Xing Yun <ink.xing.yun@gmail.com>
2018-03-22 21:43:47 -0400

    remove scientific color to percep-uni

--- src/color/scientific/index.js
+++
@@ -1,6 +0,0 @@
-export * from './palette-gen';
-export { default as reduceToPalette } from './reducer';
-export { default as getSubColorSpace } from './sub-colorspace';
-export { default as selectColorSpace } from './select-colorspace';
-export { default as hclSelector } from './util/hcl-selector';
-export { default as sortPalette } from './util/sort-palette';
